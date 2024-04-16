a152d46ef02675d48971fd7cc56d1552b154bb8c
millalinden <milla.linden@hyperisland.se>
2024-04-16 16:10:25 +0200

    Feat: Initial structure + added routing - Milla

---
+++ CHANGES.md
@@ -0,0 +1,14 @@
+We added th
+
+# Folder structure
+
+src
+| _ _ components
+            | _ _ shared 
+|
+| _ _ pages
+
+
+
+The shared folder is for reusable components for instance, buttons, links, cards etc
+The components folder is for header, footer, hero etc
---
+++ src/components/shared/Button.jsx
@@ -0,0 +1,14 @@
+// EXAMPLE OF REUSABLE BUTTON STRUCTURE
+
+// export default function Button(props) {
+//   const { onClick } = props;
+
+//   return (
+//     <button
+//       className={`padding-2 shadow-none hover:shadow background-light-${color} hover:background-dark-${color}`}
+//       onClick={onClick}
+//     >
+//       {label}
+//     </button>
+//   );
+// }
