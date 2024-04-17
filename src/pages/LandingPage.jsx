6e5b4b92478fa513e1ada2fb4eb9403bcc5e1caa
marcusryden92 <marcus.ryden1992@gmail.com>
2024-04-17 16:59:27 +0200

    Feat: added divs to section 1 and some button styling -Marcus

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,7 +1,21 @@
+import Button from "../components/shared/Button";
+
 export default function LandingPage() {
   return (
     <>
-      <section></section>
+      <section className="flex gap-12 p-32 w-full  bg-white">
+        <div className="flex flex-col gap-8 w-1/2">
+          <p>With the help of Pondeo,</p>
+          <h1 className="text-5xl font-bold">FIND AND CLOSE MORE DEALS</h1>
+          <p className="text-gray-300">
+            Transform cold outreach into engaging conversations. Pondeo blends
+            technology and a human-centric approach to redefine how you connect
+            with leads.
+          </p>
+          <Button label={"Book a Demo"} />
+        </div>
+        <div className="bg-slate-500 w-[600px] "></div>
+      </section>
       <section></section>
       <section></section>
       <section></section>
