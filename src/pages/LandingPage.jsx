b8e0de44169e76b0bfed2edbee0d5a3703136e95
marcusryden92 <marcus.ryden1992@gmail.com>
2024-04-17 17:03:24 +0200

    Fix: div sizing -Marcus

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -4,17 +4,17 @@ export default function LandingPage() {
   return (
     <>
       <section className="flex gap-12 p-32 w-full  bg-white">
-        <div className="flex flex-col gap-8 w-1/2">
+        <div className="flex flex-col py-10 gap-8 w-1/2">
           <p>With the help of Pondeo,</p>
-          <h1 className="text-5xl font-bold">FIND AND CLOSE MORE DEALS</h1>
-          <p className="text-gray-300">
+          <h1 className="text-4xl font-bold">FIND AND CLOSE MORE DEALS</h1>
+          <p className="text-gray-400">
             Transform cold outreach into engaging conversations. Pondeo blends
             technology and a human-centric approach to redefine how you connect
             with leads.
           </p>
           <Button label={"Book a Demo"} />
         </div>
-        <div className="bg-slate-500 w-[600px] "></div>
+        <div className="bg-slate-500 w-[400px] h-[400px] "></div>
       </section>
       <section></section>
       <section></section>
