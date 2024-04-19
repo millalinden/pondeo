e5a4de83d01b6b8b0f926f201ad9a08bcb5fbc88
Angela Naseri <f.naseri1993@gmail.com>
2024-04-19 18:33:46 +0200

    fix bugs

--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -1,14 +1,15 @@
-// EXAMPLE OF REUSABLE BUTTON STRUCTURE
+import React from "react";
 
-// export default function Button(props) {
-//   const { onClick } = props;
+export default function Button(props) {
+  const { onClick, label, color } = props;
 
-//   return (
-//     <button
-//       className={`padding-2 shadow-none hover:shadow background-light-${color} hover:background-dark-${color}`}
-//       onClick={onClick}
-//     >
-//       {label}
-//     </button>
-//   );
-// }
+  return (
+    <button
+      className={`py-2 px-4 border border-slate-800 ${color ? color : ""
+        } shadow-none rounded-full  hover:shadow  hover:bg-slate-300`}
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  );
+}
