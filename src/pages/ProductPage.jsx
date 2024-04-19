f7d291ebcd4183016fc5044d0bba1fc18d5d2120
Angela Naseri <f.naseri1993@gmail.com>
2024-04-19 16:49:59 +0200

    modify carousel and it's data

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -9,38 +9,39 @@ export default function ProductPage() {
     <>
     {/* pondeo section */}
 
-      <section className="flex gap-12 p-24 w-full  bg-slate-400">
+      <section className="flex gap-12 p-24 w-full  bg-black">
         <div className="flex flex-col py-10 gap-8 w-1/2">
-          <h1 className="text-5xl font-bold">PONDEO</h1>
-          <h2 className="text-4xl font-bold">Unlock the Power of Personalized Sales Outreach </h2>
+          <h1 className="text-5xl text-white font-bold">PONDEO</h1>
+          <h2 className="text-4xl text-white font-bold">Unlock the Power of Personalized Sales Outreach </h2>
           <p className="text-gray-400">
           Elevate your sales strategy with personalized messaging powered by AI insights. Connect with prospects more effectively and drive higher conversions. Start revolutionizing your outreach today!
           </p>
           <div className="flex justify-flex-start">
-            <squareButton label={"SignIn"} />
+            <Button label={"Book a Demo"} color={"bg-black text-white"}/>
           </div>
         </div>
         <div className="bg-slate-500 w-[549px] h-[676px] rounded-md"></div>
       </section>
 
       {/* benefits section */}
-      <section className="flex gap-64 py-120 px-112 w-full  bg-white">
-      <div className="text-left">
-        <h1 className="text-5xl font-bold text-gray-900">Benefits</h1>
+      <div className="text-left bg-black px-24">
+        <h1 className=" text-5xl font-bold text-lime-500">Benefits</h1>
       </div>
+      <section className="flex justify-center items-center gap-12 p-24 w-full  bg-black">
+      
       <div className="flex flex-col py-10 gap-8 w-1/2 w-[1216px] h-[216px]">
           <h3 className="text-3xl font-bold text-gray-900">Personalised Outreach </h3>
           <p className="text-gray-400">
           Pondeo personalises the cold outreach process, ensuring every communication feels unique and tailored to the recipient.          </p>
           <div className="flex justify-flex-start">
-            <Button label={"CTA"}/>
+            <Button label={"Contact Us"} color={"bg-black text-white"}/>
           </div>
       </div>
       <div className="bg-slate-300 w-[576px] h-[216px] rounded-md"></div>
       </section>
 
       {/* features section */}
-      <section className="mt-10 flex justify-center items-center">
+      <section className="mt-10 flex justify-center items-center bg-#18181b">
         <div className="bg-white w-full py-16 px-20">
             <div className="text-left">
                 <h1 className="text-5xl p-2 mx-10 mb-4 text-gray-900">Funcionlites/Features</h1>
@@ -61,7 +62,7 @@ export default function ProductPage() {
       <section className="mt-10 flex justify-center items-center">
         <div  className="bg-white w-full py-16 px-20">
             <div className="text-left">
-                <h1 className="text-5xl p-2 mx-10 mb-4 text-gray-900">How it works</h1>
+                <h1 className="text-5xl p-2 mx-10 mb-4 text-#CAFF33">How it works</h1>
 
             </div>
             <hr className="border-slate-200 mb-7 mx-9" />
@@ -75,7 +76,7 @@ export default function ProductPage() {
       </section>
 
       {/* testimonial section */}
-      <section>
+      <section className="bg-gray-400 m-auto pt-11">
         <Carousel />
 
       </section>
---
+++ src/testimonialsData/data.js
@@ -0,0 +1,45 @@
+const data = [
+    {
+        id: 1,
+        img:'',
+        star:'',
+        comment: 'Pandeo is the best',
+        name: 'Jack Smith'
+        
+    },
+    {
+        id: 2,
+        img:'',
+        star:'',
+        comment: 'Pandeo is the best',
+        name: 'Jack Smith'
+        
+    },
+    {
+        id: 3,
+        img:'',
+        star:'',
+        comment: 'Pandeo is the best',
+        name: 'Jack Smith'
+        
+    },
+    {
+        id: 4,
+        img:'',
+        star:'',
+        comment: 'Pandeo is the best',
+        name: 'Jack Smith'
+        
+    },
+    {
+        id: 5,
+        img:'',
+        star:'',
+        comment: 'Pandeo is the best',
+        name: 'Jack Smith'
+        
+    }
+];
+
+
+export default data;
