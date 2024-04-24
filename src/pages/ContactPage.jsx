cb65eeee418173ceab1e10ddea7d43d16041b7c6
PoppyRed91 <pfercek@gmail.com>
2024-04-24 19:55:45 +0200

    fix: contact page - Petra
    width, height, fonts and button component

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,4 +1,4 @@
-import { Button } from "../components/shared/Button";
+import Button from "../components/shared/Button";
 
 export default function ContactPage() {
   return (
@@ -9,30 +9,30 @@ export default function ContactPage() {
           <p className="text-custom-body-2 text-adventure-white">Any questions or remarks? Just write us a message!</p>
         </div>
 
-        <form className="flex flex-col text-adventure-white mt-[40px]">
-          <label className="flex flex-col  w-[583px] h-[50px] flex-shrink-0">First Name
-            <input type="text" className="bg-deep-black rounded-[5px] text-white border border-white ">
+        <form className="flex flex-col text-adventure-white mt-[40px] text-custom-body-1">
+          <label className="flex flex-col flex-shrink-0">First Name
+            <input type="text" className="bg-deep-black rounded-[5px] text-white border border-white h-[50px]  w-[583px] ">
             </input>
           </label>
-          <label className="mt-[15px] flex flex-col  w-[583px] h-[50px] flex-shrink-0">Last Name
-            <input type="text" className="bg-deep-black rounded-[5px] text-white border border-white ">
+          <label className="mt-[15px] flex flex-col  flex-shrink-0">Last Name
+            <input type="text" className="bg-deep-black rounded-[5px] text-white border border-white h-[50px]  w-[583px] ">
             </input>
           </label>
-          <label className="mt-[15px] flex flex-col  w-[583px] h-[50px] flex-shrink-0">Phone Number
-            <input type="text" className="bg-deep-black rounded-[5px] text-white border border-white ">
+          <label className="mt-[15px] flex flex-col flex-shrink-0">Phone Number
+            <input type="text" className="bg-deep-black rounded-[5px] text-white border border-white h-[50px] w-[583px]  ">
             </input>
           </label>
-          <label className="mt-[15px] flex flex-col  w-[583px] h-[50px] flex-shrink-0">Message
-            <input type="text" className="bg-deep-black rounded-[5px] text-white border border-white h-[100px] ">
+          <label className="mt-[15px] flex flex-col flex-shrink-0">Message
+            <input type="text" className="bg-deep-black rounded-[5px] text-white border border-white h-[100px] w-[583px]  ">
             </input>
           </label>
-          <Button
-            label={"Send Message"}
-            color={
-              "border-2 border-neon-green text-neon-green bg-none hover:bg-neon-green hover:text-black"
-            }
-          />
         </form>
+        <Button
+          label={"Send Message"}
+          color={
+            "border-2 border-neon-green text-neon-green bg-none hover:bg-neon-green hover:text-black mt-[64px]"
+          }
+        />
       </section>
     </>
   )
