fbdb0b1dcc8d28eab7fdf0ab5885c49f337de6f7
Angela Naseri <f.naseri1993@gmail.com>
2024-04-24 10:00:17 +0200

    feat: add pics

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -12,6 +12,11 @@ import HIW1 from "../assets/HIW-user input.jpeg"
 import HIW2 from "../assets/HIW-AI-based.jpeg"
 import HIW3 from "../assets/HIW-Lead Integration.jpeg"
 import HIW4 from "../assets/HIW-Personalized.jpeg"
+import Outreach from "../assets/Outreach.png"
+import productHero from "../assets/productHero.mp4"
+import efficiency from "../assets/efficiency.png"
+import dataDriven from "../assets/dataDriven.png"
+import reply from "../assets/reply.png"
 
 
 
@@ -22,18 +27,18 @@ export default function ProductPage() {
     <>
     {/* pondeo section */}
 
-      <section className="flex gap-12 p-24 w-full  bg-black">
+      <section className="flex gap-12 p-16 w-full  bg-black">
         <div className="flex flex-col py-10 gap-8 w-1/2">
           <h1 className="text-5xl text-white font-bold">PONDEO</h1>
           <h2 className="text-4xl text-white font-bold">Unlock the Power of <span className="text-neon-green">Personalized Sales</span>  Outreach </h2>
           <p className="text-gray-400">
-          Elevate your sales strategy with personalized messaging powered by AI insights. Connect with prospects more effectively and drive higher conversions. Start revolutionizing your outreach today!
-          </p>
+          Elevate your sales strategy. Transform large-scale cold outreach into personal, engaging conversations. Pondeo combines technology with a human-centric approach to enhance lead connections
+           </p>
           <div className="flex justify-flex-start">
             <Button label={"Free Trial"} color={"bg-black text-white"}/>
           </div>
         </div>
-        <div className="bg-slate-500 w-[549px] h-[676px] rounded-md"></div>
+        <div className= "w-[549px] h-[676px] rounded-lg"><iframe className="w-full aspect-square " src={productHero}></iframe></div>
       </section>
 
       <section className="flex justify-center p-10 bg-neutral-900 border-t-4 ">
@@ -51,90 +56,82 @@ export default function ProductPage() {
       </section>
 
       {/* benefits section */}
-      <div className="text-left bg-black px-24">
-        <h1 className=" text-5xl font-bold text-lime-500">Benefits</h1>
-      </div>
-      <section className="flex justify-center items-center gap-12 p-24 w-full  bg-black">
+      <section className="flex justify-center items-center gap-12 mx-16 my-4 p-9 w-full  bg-black">
       
       <div className="flex flex-col py-10 gap-8 w-1/2 w-[1216px] h-[216px]">
-          <h3 className="text-3xl font-bold text-gray-900">Personalised Outreach </h3>
-          <p className="text-gray-400">
-          Pondeo personalises the cold outreach process, ensuring every communication feels unique and tailored to the recipient.          
-          </p>
+          <h3 className="text-3xl font-bold text-neon-green">Personalised Outreach </h3>
+          <p className="text-white">
+          Pondeo personalises the cold outreach process, ensuring every communication feels unique and tailored to the recipient!          </p>
           <div className="flex justify-flex-start">
-            <Button label={"Contact Us"} color={"bg-black text-white"}/>
+            <Button label={"Book a Demo"} color={"bg-black text-white"}/>
           </div>
       </div>
-      <div className="bg-slate-300 w-[576px] h-[216px] rounded-md"></div>
+      <div className="bg-slate-300 w-[576px] h-[216px] rounded-md"><img src={Outreach}/></div>
       </section>
 
-      <section className="flex justify-center items-center gap-12 p-24 w-full  bg-black">
-      <div className="flex flex-col py-10 gap-8 w-1/2 w-[1216px] h-[216px]">
-          <h3 className="text-3xl font-bold text-gray-900">Improved Lead Conversion </h3>
-          <p className="text-gray-400">
-          The personalized approach and suggested responses help nurture leads through the sales process, leading to higher conversion rates. 
-          </p>         
-          <div className="flex justify-flex-start">
-            <Button label={"Contact Us"} color={"bg-black text-white"}/>
+
+      <section className="flex justify-center items-center bg-deep-black px-16 py-4 gap-y-28 w-full">
+        <div className="flex gap-4 p-4 h-72 ">
+          <div className="flex flex-col justify-center items-center">
+            <h3 className="custom-subtitle-1 text-neon-green">Efficiency and Workflow Streamlining</h3>
+            <p className="custom-body-1 adventure-white">Sales teams value time efficiency. 
+Pondeo automates message 
+generation, freeing up 
+sales reps to focus on 
+high-value tasks.</p>
+
           </div>
-      </div>
-      <div className="bg-slate-300 w-[576px] h-[216px] rounded-md"></div>
+          <img src={efficiency}/>
+        </div>
+
+        <div className="flex gap-4 p-4  h-72">
+        <div className="flex flex-col justify-center items-center">
+            <h3 className="custom-subtitle-1 text-neon-green">Data-Driven Insights </h3>
+            <p className="custom-body-1 adventure-white">Informed decisions drive success. Pondeo offers valuable insights,
+enabling sales teams to
+optimize strategies 
+based on real-time
+feedback. </p>
+
+          </div>
+          <img src={dataDriven}/>
+        </div>
+
       </section>
 
-      <section className="flex justify-center items-center gap-12 p-24 w-full  bg-black">
-      
-      <div className="flex flex-col py-10 gap-8 w-1/2 w-[1216px] h-[216px]">
-          <h3 className="text-3xl font-bold text-gray-900">Streamlined Sales Workflow  </h3>
-          <p className="text-gray-400">
-          Automates message generation, saving time for sales teams and improving efficiency. 
-          </p>         
-          <div className="flex justify-flex-start">
-            <Button label={"Contact Us"} color={"bg-black text-white"}/>
+      <section className="flex justify-center items-center bg-deep-black w-full p-16">
+        <div className="flex justify-center items-center gap-4 p-9">
+          <img src={reply}/>
+          <div className="flex flex-col justify-center items-center" >
+            <h2 className="custom-heading-2-bold text-neon-green">
+            Increased Open Rates and Reply Rates
+            </h2>
+            <p className="custom-subtitle-1 adventure-white">
+            Open rates by up to 50% 
+
+            Achieve reply rates as high as 15%
+            </p>
+            <button></button>
+
           </div>
-      </div>
-      <div className="bg-slate-300 w-[576px] h-[216px] rounded-md"></div>
+
+        </div>
+
       </section>
 
-      <section className="flex justify-center items-center gap-12 p-24 w-full  bg-black">
-      
-      <div className="flex flex-col py-10 gap-8 w-1/2 w-[1216px] h-[216px]">
-          <h3 className="text-3xl font-bold text-gray-900"> Insightful Analytics </h3>
-          <p className="text-gray-400">
-          Offers insights into lead responses and engagement metrics, allowing for data-driven decision-making and optimization of sales strategies. 
-          </p>         
-          <div className="flex justify-flex-start">
-            <Button label={"Contact Us"} color={"bg-black text-white"}/>
-          </div>
-      </div>
-      <div className="bg-slate-300 w-[576px] h-[216px] rounded-md"></div>
-      </section>
 
-      <section className="flex justify-center items-center gap-12 p-24 w-full  bg-black">
-      
-      <div className="flex flex-col py-10 gap-8 w-1/2 w-[1216px] h-[216px]">
-          <h3 className="text-3xl font-bold text-gray-900">Building Meaningful Connections </h3>
-          <p className="text-gray-400">
-          By understanding the person you’re talking to and genuinely caring about what’s important to them, Pondeo fosters authentic interactions.    
-          </p>       
-          <div className="flex justify-flex-start">
-            <Button label={"Contact Us"} color={"bg-black text-white"}/>
-          </div>
-      </div>
-      <div className="bg-slate-300 w-[576px] h-[216px] rounded-md"></div>
-      </section>
 
       {/* features section */}
-      <section className="mt-10 flex justify-center items-center bg-#18181b">
-        <div className="bg-white w-full py-16 px-20">
-            <div className="text-left">
-                <h1 className="text-5xl p-2 mx-10 mb-4  text-neon-green">Funcionlites/Features</h1>
-                <p className="text-gray-400">many</p>
+      <section className="mt-10 flex justify-center items-center bg-deep-black">
+        <div className=" w-full py-16 px-20">
+            <div className="text-center">
+                <h1 className="custom-heading-2-bold adventure-white p-2 mx-10 mb-4">Our Product <span className="text-neon-green">Features</span></h1>
 
             </div>
             <hr className="border-slate-200 mb-7 mx-9" />
             <div className="flex justify-center items-center justify-around mb-10">
             <div className="max-w-64">
-        <div className="h-80 w-64 rounded-3xl mt-7 bg-black"><img src={AI}/></div>
+        <div className="h-80 w-64 rounded-3xl mt-7 bg-black object-fit"><img src={AI}/></div>
 
         <h4 className="text-lg mb-3 text-left text-neon-green">AI-Based Learning Chatbot</h4>
         <p className="text-xl text-left">
@@ -143,7 +140,7 @@ export default function ProductPage() {
       </div>
 
       <div className="max-w-64">
-      <div className="h-80 w-64 rounded-3xl mt-7 bg-black"><img src={Sales}/></div>
+      <div className="h-80 w-64 rounded-3xl mt-7 bg-black object-fit"><img src={Sales}/></div>
         <h4 className="text-lg mb-3 text-left text-neon-green">Sales Assistant </h4>
         <p className="text-xl text-left">
         A smart assistant that effortlessly crafts messages customised for your potential clients. It's like having a knowledgeable friend guiding your communication efforts. 
@@ -174,13 +171,13 @@ export default function ProductPage() {
            <div className="flex justify-center items-center justify-around mb-10">
 
            <div className="max-w-64">
-        <h4 className="text-lg mb-3 text-left">User Input and Company Information</h4>
+        <h4 className="text-lg mb-3 text-left">User  Input and Company Information</h4>
         <p className="text-xl text-left">
                Users provide information about their company, including details about their products or services, target audience, and messaging preferences. You can also inform us about the main benefits your company offers or the problems you solve.
 
                This information is input into the system, allowing Pondeo to understand the company's goals and offerings.
         </p>
-        <div className="h-80 w-64 rounded-3xl mt-7 bg-black"><img src={HIW1}/></div>
+        <div className="h-80 w-64 rounded-3xl mt-7 bg-black object-fit"><img src={HIW1}/></div>
       </div>
 
       <div className="max-w-64">
@@ -190,7 +187,7 @@ export default function ProductPage() {
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1500s
         </p>
-        <div className="h-80 w-64 rounded-3xl mt-7 bg-black"><img src={HIW2}/></div>
+        <div className="h-80 w-64 rounded-3xl mt-7 bg-black "><img className="object-fill" src={HIW2}/></div>
       </div>
 
       <div className="max-w-64">
