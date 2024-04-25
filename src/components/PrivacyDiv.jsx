0514827c785080090a95c5837f60f2a5f15f046c
PoppyRed91 <pfercek@gmail.com>
2024-04-25 19:45:45 +0200

    fix: PrivacyPage - Petra - finished
    margins, padding, fonts in privacy component

--- src/components/PrivacyDiv.jsx
+++ src/components/PrivacyDiv.jsx
@@ -1,9 +1,9 @@
 export default function PrivacyDiv({ number, title, description }) {
     return (
-        <div>
-            <p className="text-[36px] text-adventure-white font-[500]">{number}</p>
-            <p className="text-[36px] text-adventure-white font-[500]">{title}</p>
-            <p className="text-[24px] text-adventure-white font-[300]">{description}</p>
+        <div className="flex flex-col gap-[12px]">
+            <p className="text-[36px] text-adventure-white font-normal text-center ">{number}</p>
+            <p className="text-[36px] text-adventure-white font-normal text-center">{title}</p>
+            <p className="text-[24px] text-adventure-white font-light">{description}</p>
         </div>
     )
 }
