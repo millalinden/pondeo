8e378577ef6aaf6f36057f2ab4df3d3b1318cc75
Angela Naseri <104205840+angnas@users.noreply.github.com>
2024-04-26 22:34:18 +0200

    fix: styles

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,8 +1,12 @@
 import { Link } from "react-router-dom";
 
-import TextImageCard from "../components/shared/TextImageCard";
 import StepSquare from "../components/shared/stepSquare";
 import ButtonLarge from "../components/shared/ButtonLarge";
+import Hero from "../components/shared/Hero";
+import Section from "../components/shared/Section";
+import ProductCard from "../components/shared/ProductCard";
+
+
 
 
 import AI from "../assets/benefits,AI.png"
@@ -37,8 +41,23 @@ export default function ProductPage() {
     return (
     <>
       {/* pondeo section */}
+      {/* <Hero styling={"bg-deep-black"} image={productHero} heightvw="40" widthvw="40">
+  
+        <h1 className="text-custom-heading-2-uppercase text-neon-green">
+        Start meaningful <br /> conversations
+        </h1>
+        <p className="text-adventure-white text-custom-subtitle-1 max-w-screen-sm">
+        With Pondeo, transform massive cold outreach into personal and engaging conversations. Pondeo blends technology and a human-centric approach to redefine how you connect with leads.
+        </p>
+        <div className="flex justify-flex-start gap-8">
+           <Link to="/demo">
+              <ButtonLarge label={"Get 7 Days Free Trial"} type={"secondary"} />
+            </Link>
+        </div>
+      </Hero> */}
 
-      <section className="flex gap-12 p-16 w-full  bg-black">
+
+      <Section styling={"justify-center gap-12 p-16 bg-deep-black border-graymatter-gray border shadow-dark-black "}>
         <div className="flex flex-col py-10 gap-8 w-1/2">
           <h2 className="text-custom-heading-2-uppercase text-neon-green">Start meaningful <br /> conversations</h2>
           <p className="text-adventure-white text-custom-subtitle-1 max-w-screen-sm">
@@ -51,47 +70,46 @@ export default function ProductPage() {
           </div>
         </div>
         {/* <div className= "w-[632px] h-[525px] rounded-lg"><iframe className="w-full aspect-square " src={productHero}></iframe></div> */}
-        <iframe className="w-[632px] h-[525px] rounded-lg  aspect-square " src={productHero}></iframe>
+        <iframe className="w-[632px] h-[525px] rounded-lg  aspect-square " src={productHero} alt=""></iframe>
 
-      </section>
+        </Section>
 
-      <section className="flex justify-center items-center p-10 bg-graymatter-gray border-t-4 ">
 
-        <div className="flex justify-center w-[1343px] h-[166px] bg-deep-black rounded-lg items-center">
+      <Section styling={"justify-center items-center p-10 bg-graymatter-gray "}>
+        <div className="flex justify-center w-[1343px] h-[166px] bg-deep-black border-graymatter-gray border shadow-dark-black rounded-lg items-center">
           <h1 className="text-custom-heading-3 text-neon-green text-center">
           Pondeo: Your AI Sales Assistant Analyzing Benefits
           </h1>
         </div>
         
-      </section>
+      </Section>
+
 
 
       {/* benefits section */}
-      <section className="flex flex-col items-center justify-center gap-16 p-9 w-full  bg-graymatter-gray">
-      
-  <div className="bg-black flex flex-col w-[1343px] rounded-lg items-center gap-6">
-  <h2 className="text-custom-heading-2-uppercase text-neon-green text-center pt-4">Personalised Outreach </h2>
+      <Section styling={"justify-center flex-col items-center gap-16 p-6 pt-4 bg-graymatter-gray"}>
+          <div className="bg-black flex flex-col w-[1343px] rounded-lg items-center gap-6  border-graymatter-gray border shadow-dark-black">
+          <h2 className="text-custom-heading-2-uppercase text-neon-green text-center pt-4">Personalised Outreach </h2>
           <p className="text-adventure-white text-custom-subtitle-1 text-center max-w-2xl">
           Pondeo customizes the cold outreach process, making every communication feel unique and tailored to the recipient!          </p>
 
           
           {/* <div className="w-[1344px] h-[899px] rounded-lg"><iframe className="w-full aspect-square " src={Outreach}></iframe></div> */}
-          <iframe className="w-[1344px] h-[899px] rounded-lg  aspect-square " src={Outreach}></iframe>
+          <iframe className="w-[1344px] h-[899px] rounded-lg  aspect-square " src={Outreach} alt=""></iframe>
           </div>
 
-      </section>
+          </Section>
 
 
-
-      <section className="flex justify-center items-center bg-graymatter-gray w-full p-9">
-        <div className="flex justify-center items-center gap-4 p-9">
-          <img className="w-[536px] h-[438px]" src={reply}/>
-          <div className="flex flex-col justify-center items-center gap-20" >
+          <Section styling={"justify-center items-center  bg-graymatter-gray p-9"}>
+        <div className="flex place-items-start  gap-4 p-9">
+          <img className="w-[536px] h-[438px]" src={reply} alt=""/>
+          <div className="flex flex-col place-items-start  gap-20" >
             <div>
-            <h2 className="text-custom-subtitle-1 text-neon-green text-left">
+            <h2 className="text-custom-subtitle-1 text-neon-green text-left pb-4">
             Increased Open Rates and Reply Rates
             </h2>
-            <p className="text-custom-body-1 text-adventure-white max-w-lg text-left">
+            <p className="text-custom-body-1 text-adventure-white max-w-lg text-left leading-10">
             Open rates by up to 50% <br /> Achieve reply rates as high as 15%
             </p>
             </div>
@@ -99,19 +117,16 @@ export default function ProductPage() {
             <ButtonLarge label={"Free Trial"} type={"secondary"} />
             </Link>
             
-
           </div>
 
         </div>
+        </Section>
 
-      </section>
+        <Section styling={"justify-center items-center bg-graymatter-gray px-16 py-4 gap-28"}>
 
-
-      <section className="flex justify-center items-center bg-graymatter-gray px-16 py-4 gap-28 w-full">
-
-        <div className="flex gap-4 p-4 h-72 w-[600px] rounded-lg bg-deep-black">
+        <div className="flex p-4 h-[271px] w-[600px] rounded-lg bg-deep-black border-graymatter-gray border shadow-dark-black">
           <div className="flex flex-col justify-center items-center">
-            <h3 className="text-custom-subtitle-1 text-neon-green">Efficiency and Workflow Streamlining</h3>
+            <h3 className="text-custom-subtitle-small text-neon-green w-[294px]">Efficiency and Workflow Streamlining</h3>
             <p className="text-custom-body-1 text-adventure-white w-71">Sales teams value time efficiency. 
 Pondeo automates message 
 generation, freeing up 
@@ -119,12 +134,12 @@ sales reps to focus on
 high-value tasks.</p>
 
           </div>
-          <img src={efficiency} />
+          <img alt="" src={efficiency} />
         </div>
 
-        <div className="flex gap-4 p-4  h-72 w-[600px] rounded-lg bg-deep-black">
+        <div className="flex p-4  h-[271px] w-[600px] rounded-lg bg-deep-black border-graymatter-gray border shadow-dark-black">
         <div className="flex flex-col justify-center items-center">
-            <h3 className="text-custom-subtitle-1 text-neon-green">Data-Driven Insights </h3>
+            <h3 className="text-custom-subtitle-1 text-neon-green ">Data-Driven Insights </h3>
             <p className="text-custom-body-1 text-adventure-white w-71">Informed decisions drive success. Pondeo offers valuable insights,
 enabling sales teams to
 optimize strategies 
@@ -132,60 +147,62 @@ based on real-time
 feedback. </p>
 
           </div>
-          <img src={dataDriven} />
+          <img alt="" src={dataDriven} />
         </div>
-      </section>
+        </Section>
 
       
 
       {/* features section */}
-      <section className="py-16 px-16 flex justify-center items-center bg-deep-black">
-        <div className=" w-full text-center py-16 px-20">
-            <div className="text-center">
-                <h1 className="text-custom-heading-2-bold text-adventure-white p-2 mx-10 mb-4">Our Product <span className="text-neon-green">Features</span></h1>
+      <Section styling={"justify-center items-center p-16  bg-deep-black"}>
+        <div className=" w-full text-center pt-4">
+            <div className="text-center ">
+                <h1 className="text-custom-heading-2-bold text-adventure-white p-2 mx-10 mb-4 pb-4">Our Product <span className="text-neon-green">Features</span></h1>
 
             </div>
-            <div className="flex justify-center items-center gap-20">
-            <TextImageCard
+            <div className="flex flex-wrap justify-center place-items-start pt-12 gap-20">
+            <ProductCard
               heading="AI-Based Learning Chatbot"
               text="Utilizes machine learning algorithms to analyze company information and learn about products, services, target audience, and messaging preferences."
               image={AI}
+              alt="AI-Based Learning Chatbot"
             />
-            <TextImageCard
+            <ProductCard
               heading="Personalized Message Generation"
               text="Pondeo uses data on preferences, company details, and activities to personalize messages. It adapts to channels, use cases, and preferred tone for effective communication."
               image={Messages}
+              alt="Personalized Message Generation"
             />
-            <TextImageCard
+            <ProductCard
               heading="Sales Assistant "
               text="A smart assistant that effortlessly crafts messages customised for your potential clients. It's like having a knowledgeable friend guiding your communication efforts."
               image={Sales}
+              alt="Sales Assistant"
             />
             
           </div>
-
-
            
         </div>
-      </section>
+        </Section>
 
-      <svg xmlns="http://www.w3.org/2000/svg" width="1312" height="2" viewBox="0 0 1312 2" fill="none">
-<path d="M0 1H1312" stroke="#DEE5ED" stroke-width="2"/>
+     
+<svg xmlns="http://www.w3.org/2000/svg" width="1312" height="3" viewBox="0 0 1312 3" fill="none">
+  <path d="M0 1.5H1312" stroke="#DEE5ED" stroke-width="2"/>
 </svg>
 
 
 
       {/* how Pondeo works section */}
-      <section className="bg-deep-black flex justify-center items-center">
-        <div  className=" w-full px-16 gap-16">
+      <Section styling={"flex-col justify-center items-center px-16 gap-16 bg-deep-black "}>
+        
             <div className="text-center">
                 <h1 className="text-custom-heading-2-bold py-16 mx-10 mb-4 text-adventure-white">How Pondeo works</h1>
 
             </div>
 
 
-            <div className="flex justify-center items-center gap-16 px-4 py-8">
-              <div className="flex flex-col justify-center items-start gap-10 max-w-xl">
+            <div className="flex gap-16 px-4 py-8 bg-deep-black border-graymatter-gray border shadow-dark-black">
+              <div className="flex flex-col  gap-10 max-w-xl">
                 <StepSquare 
                 label={"Step 1"}
                 />
@@ -194,11 +211,11 @@ feedback. </p>
 
                 <br /> This information is input into the system, allowing Pondeo to understand the company's goals and offerings.</p>
               </div>
-              <img className="w-[598px] h-[668px]" src={step1}/>
+              <img className="w-[598px] h-[668px]" src={step1} alt=""/>
             </div>
 
-            <div className="flex justify-center items-center  gap-20 px-4 py-8">
-              <img className="w-[618px] h-[481px]" src={step2}/>
+            <div className="flex  gap-20 px-4 py-8 bg-deep-black border-graymatter-gray border shadow-dark-black">
+              <img className="w-[618px] h-[481px]" src={step2} alt=""/>
               <div className="flex flex-col max-w-xl gap-8">
                 <StepSquare 
                 label={"Step 2"}
@@ -212,7 +229,7 @@ feedback. </p>
               </div>
             </div>
 
-            <div className="flex justify-center items-center gap-24 px-4 py-8">
+            <div className="flex gap-24 px-4 py-8 bg-deep-black border-graymatter-gray border shadow-dark-black">
               <div className="flex flex-col w-[539px] gap-8">
                 <StepSquare 
                 label={"Step 3"}
@@ -222,11 +239,11 @@ feedback. </p>
 
                 <br /> This action initiates the process of personalised messaging for each lead.</p>
               </div>
-              <img className="w-[626px] h-[330px]"  src={step3}/>
+              <img className="w-[626px] h-[330px]"  src={step3} alt=""/>
             </div>
 
-            <div className="flex justify-center items-center  gap-20 px-4 py-8">
-              <img className="w-[609px] h-[443px]" src={step4}/>
+            <div className="flex gap-20 px-4 py-8 bg-deep-black border-graymatter-gray border shadow-dark-black">
+              <img className="w-[609px] h-[443px]" src={step4} alt=""/>
               <div className="flex flex-col  max-w-xl gap-8">
                 <StepSquare 
                 label={"Step 4"}
@@ -238,11 +255,8 @@ feedback. </p>
               </div>
             </div>
 
-  
 
-        </div>
-
-      </section>
+            </Section>
 
 
 
